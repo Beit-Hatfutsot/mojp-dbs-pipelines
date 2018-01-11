14b7df1ff2114ccb3e32a4e381ef8908422a0adc
Ori Hoch <ori@uumpa.com>
2018-01-11 17:38:36 +0200

    .travis

--- continuous_deployment.sh
+++ continuous_deployment.sh
@@ -3,17 +3,21 @@
 HELM_UPDATE_COMMIT_MESSAGE="${K8S_ENVIRONMENT_NAME} ${PROJECT_NAME} image update --no-deploy"
 
 RES=0
+ERROR=""
 
 cd /ops
 
 ! ./helm_update_values.sh "${B64_UPDATE_VALUES}" "${HELM_UPDATE_COMMIT_MESSAGE}" "${K8S_OPS_GITHUB_REPO_TOKEN}" \
                           "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" \
-    && echo 'failed helm update values' && RES=1;
+    && ERROR="failed helm update values" && RES=1;
+
+! $HELM_UPGRADE_CMD && ERROR="failed helm upgrade" && RES=1;
 
 cd /pwd
 
 ! gcloud container builds submit --substitutions _IMAGE_TAG=${IMAGE_TAG},_CLOUDSDK_CORE_PROJECT=${CLOUDSDK_CORE_PROJECT},_PROJECT_NAME=${PROJECT_NAME} \
                                  --config continuous_deployment_cloudbuild.yaml . \
-    && echo 'failed to build ${PROJECT_NAME} image' && RES=1;
+    && ERROR="failed to build ${PROJECT_NAME} image" && RES=1;
 
+echo "${ERROR}"
 exit $RES
